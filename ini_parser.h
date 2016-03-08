105a93432f4e5e771887543f9efc98b1cec75700
jjy_8888@163.com <jjy_8888@163.com>
2016-03-08 02:15:06 -0500

    add some defines for parse header file

--- ini_parser.h
+++ ini_parser.h
@@ -1,3 +1,9 @@
+/* File: ini_parse.h
+ * Author: jack
+ * Email: jjy_8888@163.com
+ * Brief: ini parser head file
+ *
+ * */
 #ifndef _INI_PARSE_H
 #define _INI_PARSE_H
 
@@ -5,14 +11,26 @@ struct  _IniParser;
 typedef struct _IniParser IniParser;
 typedef enum _Ret
 {
-	SECTION_NOT_EXISTS,
-	KEY_NOT_EXISTS,
+	RET_OK,
+	RET_STOP,
+	RET_INVALID_PARAMS,
+	RET_FAIL
 } Ret;
-typedef void(*EntryVisitFunc)(char* section, char* key, char* val);
+
+#define return_if_fail(p) if(!(p)) \
+	{printf("%s:%d Warning: "#p" failed.\n", \
+			__func__, __LINE__); return;}
+#define return_val_if_fail(p, ret) if(!(p)) \
+	{printf("%s:%d Warning: "#p" failed.\n", \
+			__func__, __LINE__); return (ret);}
+
+#define SAFE_FREE(p) if(p != NULL){free(p); p = NULL;}
+
+typedef void(*EntryVisitFunc)(void* ctx, char* section, char* key, char* val);
 
 IniParser* ini_parser_new(char* fname);
 Ret ini_parser_get_by_key(IniParser* thiz, char* section, char* key, char** val);
-R ini_parser_foreach(IniParser* thiz, EntryVisitFunc* visit);
+Ret ini_parser_foreach(IniParser* thiz, EntryVisitFunc* visit);
 void ini_parser_destroy(IniParser* thiz);
 
 #endif
